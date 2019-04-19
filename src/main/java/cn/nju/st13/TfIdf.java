5509ff14f1a1f5c4f05a642edf92dc6a0c53695a
ztw <852445892@qq.com>
2019-04-20 00:20:14 +0800

    修复bug

--- src/main/java/cn/nju/st13/TfIdf.java
+++ src/main/java/cn/nju/st13/TfIdf.java
@@ -1,10 +1,5 @@
 package cn.nju.st13;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
@@ -20,6 +15,11 @@ import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
 import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -31,20 +31,11 @@ public class TfIdf {
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
-
         public void map(Object key, Text value, Context context)
                 throws IOException, InterruptedException {
 
             FileSplit fileSplit = (FileSplit) context.getInputSplit();
             String filename = fileSplit.getPath().getName(); //得到文件名
-/*
-            // 为每位作家计算文档数
-            boolean flag = false;
-            if(!filenames.contains(filename)) {
-                filenames.add(filename);
-                flag = true;
-            }
-*/
 
             Matcher m = p_author.matcher(filename);
             String author = "";
@@ -52,29 +43,18 @@ public class TfIdf {
                 author = m.group();
             else
                 System.err.println("提取作者名字时出错");
-            /*
-            if(flag) {
-                if (authorFileCount.containsKey(author)) {
-                    authorFileCount.put(author, authorFileCount.get(author) + 1);
-                } else {
-                    authorFileCount.put(author, 1);
-                }
-            }*/
+
 
             StringTokenizer itr = new StringTokenizer(value.toString());
             while (itr.hasMoreTokens()) {
-                word.set(author + "," + itr.nextToken() + "#" + filename);
-                context.write(word, one);
-            }
-/*
-            try {
-                if (authorFileCount.isEmpty()) {
-                    throw new Exception("hashmap为空");
+                // 过滤掉前两行
+                if(word.toString().equals("TXT") || word.toString().equals("www.txthj.com")) {
+                    return;
+                } else {
+                    word.set(author + "," + itr.nextToken() + "#" + filename);
+                    context.write(word, one);
                 }
-            } catch (Exception ex) {
-                ex.printStackTrace();
             }
-        }*/
 
         }
     }
@@ -100,7 +80,7 @@ public class TfIdf {
             extends HashPartitioner<Text, IntWritable> {
 
         public int getPartition(Text key, IntWritable value, int numReduceTasks) {
-
+            // 按照<作者名，单词>进行划分
             String term = key.toString().split("#")[0];
             return super.getPartition(new Text(term),value,numReduceTasks);
 
@@ -114,6 +94,7 @@ public class TfIdf {
         private static String currentTerm = " ";
         private static int currentSum = 0;
         private static int currentCount = 0;
+
 
         public void reduce(Text key, Iterable<IntWritable> values, Context context)
                 throws IOException, InterruptedException {
@@ -129,8 +110,10 @@ public class TfIdf {
             } else {
                 // write上一次结果
                 if(!currentTerm.equals(" ")) {
+                    Configuration conf = context.getConfiguration();
                     String author = currentTerm.split(",")[0];
-                    double tfidf = currentSum*Math.log((double)authorFileCount.get(author)/(currentCount+1));
+                    int countFile = Integer.parseInt(conf.get(author));
+                    double tfidf = currentSum*Math.log((double)countFile/(double)(currentCount+1));
                     result.set(tfidf);
                     context.write(new Text(currentTerm), result);
                 }
@@ -141,10 +124,20 @@ public class TfIdf {
 
         }
 
+        //最后一个单词
+        public void cleanup(Context context)
+                throws IOException,InterruptedException {
+            Configuration conf = context.getConfiguration();
+            String author = currentTerm.split(",")[0];
+            int countFile = Integer.parseInt(conf.get(author));
+            double tfidf = currentSum*Math.log((double)countFile/(double)(currentCount+1));
+            result.set(tfidf);
+            context.write(new Text(currentTerm), result);
+        }
+
     }
 
-    //private static HashSet<String> filenames = new HashSet<>();
-    private static HashMap<String, Integer> authorFileCount = new HashMap<>();
+
     private static Pattern p_author = Pattern.compile("^[\\u4e00-\\u9fa5]*"); //从文件名中提取作者
 
     public static void main(String[] args) throws Exception {
@@ -161,6 +154,7 @@ public class TfIdf {
         // 获取作家与作品总数的对应关系
         FileSystem fs = FileSystem.get(new Configuration());
         FileStatus[] status = fs.listStatus(new Path(args[0]));
+        HashMap<String, Integer> authorFileCount = new HashMap<>();
         for (FileStatus file : status) {
             String filename = file.getPath().getName();
             Matcher m = p_author.matcher(filename);
@@ -174,11 +168,12 @@ public class TfIdf {
                 authorFileCount.put(author, 1);
             }
         }
-        System.out.println(authorFileCount);
+        // 通过设置Job的Configuration来共享变量
+        for (Map.Entry<String, Integer> entry : authorFileCount.entrySet()) {
+            conf.set(entry.getKey(), entry.getValue().toString());
+        }
 
-        Job job = new Job();
-        job.setJarByClass(TfIdf.class);
-        job.setJobName("tfidf app");
+        Job job = Job.getInstance(conf, "tfidf job");
         job.setJarByClass(TfIdf.class);
         job.setMapperClass(TfIdfMapper.class);
         job.setCombinerClass(TfIdfCombiner.class);
