4c710b34f302fff0fac1a92a1988286c5a1b90b8
BYGX-wcr <wcr@live.cn>
2019-04-20 15:39:53 +0800

    test & fix part2

--- src/main/java/cn/nju/st13/WordFreqSort.java
+++ src/main/java/cn/nju/st13/WordFreqSort.java
@@ -24,9 +24,9 @@ import java.util.*;
 正常情况下先使用文档倒排程序对文件进行处理，再对词频进行排序
 如果命令行中在文件路径前加入-direct参数则会直接排序
 */
-public class WordFreqSort {    
+public class WordFreqSort {
     //Job2 Mapper
-    public static class WordFreqSortMapper extends Mapper<Object, Text, Text, FloatWritable> {
+    public static class WordFreqSortMapper extends Mapper<Object, Text, FloatWritable, Text> {
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             String input = value.toString();
             String[] lines = input.split("\n");
@@ -35,16 +35,16 @@ public class WordFreqSort {
                 String[] lineCompent = str.split("\t");
                 String[] content = lineCompent[1].split(",");
 
-                context.write(new Text(lineCompent[0]), new FloatWritable(new Float(content[0])));
+                context.write(new FloatWritable(new Float(content[0])), new Text(lineCompent[0]));
             }
         }
     }
 
     //Job2 Mapper
-    public static class WordFreqSortReducer extends Reducer<Text, FloatWritable, Text, Text> {
-        public void reduce(Text key, Iterable<FloatWritable> values, Context context) throws IOException, InterruptedException {
-            for (FloatWritable val : values) {
-                context.write(key, new Text(":" + val.toString() + "\n"));
+    public static class WordFreqSortReducer extends Reducer<FloatWritable, Text, Text, Text> {
+        public void reduce(FloatWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            for (Text val : values) {
+                context.write(val, new Text(":" + key.toString()));
             }
         }
     }
@@ -52,7 +52,7 @@ public class WordFreqSort {
 	public static void main(String[] args) throws Exception{
         Configuration configuration = new Configuration();
         
-        if (args[0] != "-direct") {
+        if (!args[0].equals("-direct")) {
             Job job1 = Job.getInstance(configuration, "2019St13 Word Frequency Sort Job1");
             job1.setJarByClass(WordFreqSort.class);
             job1.setMapperClass(InvertedIndexer.InvertedIndexMapper.class);
@@ -73,12 +73,12 @@ public class WordFreqSort {
                 job2.setReducerClass(WordFreqSortReducer.class);
                 job2.setPartitionerClass(TotalOrderPartitioner.class);
                 job2.setInputFormatClass(TextInputFormat.class);
-                job2.setOutputKeyClass(Text.class);
+                job2.setOutputKeyClass(FloatWritable.class);
                 job2.setOutputValueClass(Text.class);
-                job2.setNumReduceTasks(5);
                 FileInputFormat.addInputPath(job2, new Path(args[1] + ".tempout"));
                 FileOutputFormat.setOutputPath(job2, new Path(args[1]));
 
+                job2.waitForCompletion(true);
                 FileSystem fs =new Path(args[1] + ".tempout").getFileSystem(configuration);
                 if(fs.exists(new Path(args[1] + ".tempout"))){
                       fs.delete(new Path(args[1] + ".tempout"), true); 
@@ -94,11 +94,12 @@ public class WordFreqSort {
             job2.setReducerClass(WordFreqSortReducer.class);
             job2.setPartitionerClass(TotalOrderPartitioner.class);
             job2.setInputFormatClass(TextInputFormat.class);
-            job2.setOutputKeyClass(Text.class);
-            job2.setOutputValueClass(IntWritable.class);
-            job2.setNumReduceTasks(5);
-            FileInputFormat.addInputPath(job2, new Path(args[0]));
-            FileOutputFormat.setOutputPath(job2, new Path(args[1]));
+            job2.setOutputKeyClass(FloatWritable.class);
+            job2.setOutputValueClass(Text.class);
+            FileInputFormat.addInputPath(job2, new Path(args[1]));
+            FileOutputFormat.setOutputPath(job2, new Path(args[2]));
+
+            System.exit(job2.waitForCompletion(true) ? 0 : 1); 
         }
 	}
 }
